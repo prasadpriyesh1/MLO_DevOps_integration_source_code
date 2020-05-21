5fe2d54e29a230576d7ae4dd46cbafeae34d9e28
Priyesh Prasad <prasadpriyesh1@gmail.com>
2020-05-21 12:55:18 +0530

    nine commit

--- check_accuracy.py
+++ check_accuracy.py
@@ -5,7 +5,7 @@ Created on Thu May 21 11:46:16 2020
 @author: prasa
 """
 from sys import exit
-file = open("D:\\MLOps\\MLOps_docker_integration_code\\accuracy.txt")
+file = open("accuracy.txt")
 str = file.readline()
 n = float(str)
 
